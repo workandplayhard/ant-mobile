714065fdbb58eca52ba56d7eaff4de94ddf65219
gabriel-cyberfuze <--global>
2022-12-12 03:34:31 -0800

    fixed Icon component & added icons svg

--- src/components/Icon/icons.ts
+++ src/components/Icon/icons.ts
@@ -1,5 +1,65 @@
+import tvIcon from '@/assets/icons/ic_tv.svg'
+☻import vipIcon from '@/assets/icons/ic_vip.svg'
+import carIcon from '@/assets/icons/ic_car.svg'
+import bankIcon from '@/assets/icons/ic_bank.svg'
+import menuIcon from '@/assets/icons/ic_menu.svg'
+import ringIcon from '@/assets/icons/ic_ring.svg'
+import mailIcon from '@/assets/icons/ic_mail.svg'
+import prayIcon from '@/assets/icons/ic_pray.svg'
+import checkIcon from '@/assets/icons/ic_check.svg'
+import humanIcon from '@/assets/icons/ic_human.svg'
+import closeIcon from '@/assets/icons/ic_close.svg'
+import musicIcon from '@/assets/icons/ic_music.svg'
+import energyIcon from '@/assets/icons/ic_energy.svg'
+import cuteTVIcon from '@/assets/icons/ic_cute_tv.svg'
 import messageIcon from '@/assets/icons/ic_message.svg'
+import accountIcon from '@/assets/icons/ic_account.svg'
+import arrowUpIcon from '@/assets/icons/ic_arrow_up.svg'
+import showCVVIcon from '@/assets/icons/ic_show_cvv.svg'
+import settingsIcon from '@/assets/icons/ic_settings.svg'
+import downloadIcon from '@/assets/icons/ic_download.svg'
+import arrowNextIcon from '@/assets/icons/ic_arrow_next.svg'
+import arrowDownIcon from '@/assets/icons/ic_arrow_down.svg'
+import arrowPrevIcon from '@/assets/icons/ic_arrow_prev.svg'
+import arrowBackIcon from '@/assets/icons/ic_arrow_back.svg'
+import balloonHotIcon from '@/assets/icons/ic_balloon_hot.svg'
+import notificationIcon from '@/assets/icons/ic_notification.svg'
+import balloonThinIcon from '@/assets/icons/ic_balloon_thin.svg'
+import tvDefaultPlanIcon from '@/assets/icons/ic_tv_default_plan.svg'
+import copyCardNumberIcon from '@/assets/icons/ic_copy_card_number.svg'
+import walletCreditCardIcon from '@/assets/icons/ic_wallet_credit_card.svg'
+import handsHoldingCircleIcon from '@/assets/icons/ic_hands_holding_circle.svg'
 
 export default {
+  tvIcon,
+  carIcon,
+  vipIcon,
+  bankIcon,
+  menuIcon,
+  ringIcon,
+  mailIcon,
+  prayIcon,
+  checkIcon,
+  humanIcon,
+  closeIcon,
+  musicIcon,
+  energyIcon,
+  cuteTVIcon,
+  showCVVIcon,
   messageIcon,
+  accountIcon,
+  arrowUpIcon,
+  settingsIcon,
+  downloadIcon,
+  arrowNextIcon,
+  arrowPrevIcon,
+  arrowDownIcon,
+  arrowBackIcon,
+  balloonHotIcon,
+  balloonThinIcon,
+  notificationIcon,
+  tvDefaultPlanIcon,
+  copyCardNumberIcon,
+  walletCreditCardIcon,
+  handsHoldingCircleIcon,
 }
