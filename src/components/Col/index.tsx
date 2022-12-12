1e0671bc50ffc91c3795ab764ab16592acf815c3
gabriel-cyberfuze <--global>
2022-12-12 04:13:42 -0800

    fixed Col component

--- src/components/Col/index.tsx
+++ src/components/Col/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import { View, StyleProp, TextStyle, Text } from 'react-native'
+import { View, StyleProp, ViewStyle } from 'react-native'
 
 import styles from './styles'
 
 interface ICol {
   children: React.ReactNode
-  style?: StyleProp<TextStyle>
+  style?: StyleProp<ViewStyle>
 }
 
 export const Col: React.FC<ICol> = ({ style = {}, children }) => {
-  return (
-    <View>
-      <Text style={[styles.container, style]}>{children}</Text>
-    </View>
-  )
+  return <View style={[styles.container, style]}>{children}</View>
 }
