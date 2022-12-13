843df864dd6d38756d53ec63a8cdc38dece6d351
fatpig0416 <fatpig0416@gmail.com>
2022-12-13 16:57:30 +0800

    add linear gradieng, update Button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,16 +1,50 @@
-import { StyleSheet } from 'react-native'
+import { I18nManager, StyleSheet } from 'react-native'
 
-import { font, RH } from '@/theme/utils'
+import { WHITE, font, RH, RW } from '@/theme'
 
 export default StyleSheet.create({
   wrapper: {
     width: '100%',
+  },
+  background: {
+    width: '100%',
+    borderWidth: RW(1),
+    borderColor: 'transparent',
+    flexDirection: I18nManager.isRTL ? 'row-reverse' : 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    overflow: 'hidden',
+    borderRadius: RW(30),
+    width: '100%',
+  },
+  xs: {
+    borderRadius: RW(32),
+    paddingVertical: RH(10),
+  },
+  sm: {
+    borderRadius: RW(30),
     paddingVertical: RH(12),
   },
-  textStyle: {
-    ...font('rw.regular', 18, 'white', 30),
+  md: {
+    borderRadius: RW(30),
+    paddingVertical: RH(14),
   },
-  buttonStyle: {
-    width: '100%',
+  lg: {
+    borderRadius: RW(32),
+    paddingVertical: RH(18),
+  },
+  xsText: {
+    ...font('rw.semibold', 14, WHITE, 20),
+  },
+  smText: {
+    ...font('rw.semibold', 16, WHITE, 24),
+  },
+  mdText: {
+    ...font('rw.semibold', 12, WHITE, 14),
+  },
+  lgText: {
+    ...font('rw.semibold', 18, WHITE, 30),
   },
 })
