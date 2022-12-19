1db682ed1cdee333af75883438411e3296a8f82a
gabriel-cyberfuze <--global>
2022-12-19 13:30:50 -0800

    fixed style

--- src/navigation/styles.ts
+++ src/navigation/styles.ts
@@ -6,10 +6,11 @@ export default StyleSheet.create({
   container: {
     alignItems: 'center',
     top: RH(5),
+    width: '100%',
   },
   border: {
     position: 'absolute',
-    width: RW(100),
+    width: '100%',
     height: RH(8),
     bottom: RW(-6),
     borderTopLeftRadius: RW(15),
