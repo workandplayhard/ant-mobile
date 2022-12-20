f1a4e8b14390578b08753fa239ad79eed3832723
gabriel-cyberfuze <--global>
2022-12-20 05:23:07 -0800

    added Nabbar component

--- src/components/Row/styles.ts
+++ src/components/Row/styles.ts
@@ -3,7 +3,7 @@ import { I18nManager, StyleSheet } from 'react-native'
 export default StyleSheet.create({
   container: {
     width: '100%',
-    flexDirection: I18nManager.isRTL ? 'row' : 'row',
+    flexDirection: I18nManager.isRTL ? 'row-reverse' : 'row',
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
