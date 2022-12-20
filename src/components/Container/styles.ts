703b1cc68f4c99da1348cba2c490e1a3990be434
gabriel-cyberfuze <--global>
2022-12-20 07:24:20 -0800

    added Container component

---
+++ src/components/Container/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from '@/constants'
+
+export default StyleSheet.create({
+  container: {
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+    flexDirection: 'column',
+  },
+})
